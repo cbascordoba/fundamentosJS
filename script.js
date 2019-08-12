5ec98cb634965d68aa3b15ddda2041e2a51ca3f7
cbascordoba14 <cbascordoba14@gmail.com>
2019-08-11 23:09:27 -0500

    convetir a mayúsculas un texto y traer una subcadena

---
+++ script.js
@@ -0,0 +1,16 @@
+var nombre='Sebastián', apellido='Córdoba'
+var nombreMayuscula= nombre.toUpperCase()
+
+var apellidoMinuscula =apellido.toLowerCase()
+
+var primeraLetra = nombre.charAt(0)
+
+var cantidadLetras = nombre.length
+
+var nombreCompleto =   ` ${nombre} ${apellido} El mas`
+
+//var str = nombre.charAt(1) + nombre.charAt(2)
+
+var str = nombre.substr(1,2)
+
+var ultimaLetra = nombre.charAt(nombre.length-1)
