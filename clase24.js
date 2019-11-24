e7bc07f3965b5c5e7d70a928a48825526aee6da9
cbascordoba14 <cbascordoba14@gmail.com>
2019-11-24 00:18:25 -0500

    24/25 dentro de una arrow function, this esta haciendo referencia al espacio global , a windows

---
+++ clase24.js
@@ -0,0 +1,21 @@
+function Persona(nombre, apellido, altura){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+Persona.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+// 25. dentro de una arrow function, this está haciendo referencia al espacio global, a windows.
+Persona.prototype.soyAlto = function(){
+    if(this.altura >= 1.8){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy alto`)
+    }
+    else{
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy bajo`)
+    }
+}
+var sebas = new Persona('Sebas','Cordoba',1.66)
+sebas.saludar()
+sebas.soyAlto()
